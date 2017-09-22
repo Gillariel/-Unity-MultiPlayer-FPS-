d15f918bcb1b8c1b9f64762640efcc6fe24cd5e4
DesktopGillariel <foerster.nathan@hotmail.com>
2017-09-22 20:17:36 +0200

    Player movement sync

---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    Camera sceneCamera;
+
+	// Use this for initialization
+	void Start () {
+        // Object is not the host
+        if (!isLocalPlayer)
+            foreach (Behaviour b in componentsToDisable)
+                b.enabled = false;
+        else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+                sceneCamera.gameObject.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDisable()
+    {
+        if (sceneCamera != null)
+            sceneCamera.gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+
+    [SerializeField]
+    private float mouseSensibility = 3f;
+
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+
+    }
+
+    void Update()
+    {
+        float _xMove = Input.GetAxisRaw("Horizontal");
+        float _zMove = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = transform.right * _xMove;
+        Vector3 _moveVertical = transform.forward * _zMove;
+
+        //Final movement vector
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+
+        motor.Move(_velocity);
+
+        //Rotation
+        float yRota = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rota = new Vector3(0f, yRota, 0f) * mouseSensibility;
+
+        //Apply rotation
+        motor.Rotate(_rota);
+
+        //Camera Rotation
+        float xRota = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRota = new Vector3(xRota, 0f, 0f) * mouseSensibility;
+
+        //Apply rotation
+        motor.RotateCamera(_cameraRota);
+    }
+}
